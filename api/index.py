6bdc5649a22b774b81e53f7f541a7c284e9ffa6f
JIAMIJIAOYIJIAOSHOU <mallqy2657761@gmail.com>
2026-03-15 21:55:45 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,16 +1,9 @@
-from flask import Flask, render_template
-import os
+from flask import Flask
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-app = Flask(
-    __name__,
-    template_folder=os.path.join(BASE_DIR, "templates"),
-    static_folder=os.path.join(BASE_DIR, "static")
-)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "<h1>AI Stock Analyzer Running</h1>"
 
-handler = app
+# Vercel 会自动识别 app 对象
