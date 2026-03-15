793801c13d66cf90858482588a318c9fa961da57
JIAMIJIAOYIJIAOSHOU <mallqy2657761@gmail.com>
2026-03-15 20:40:52 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__, template_folder="../templates", static_folder="../static")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+app = Flask(
+    __name__,
+    template_folder=os.path.join(BASE_DIR, "templates"),
+    static_folder=os.path.join(BASE_DIR, "static")
+)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# Vercel 需要这个变量
 handler = app
