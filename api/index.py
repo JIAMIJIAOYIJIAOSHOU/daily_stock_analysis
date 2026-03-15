5e6cb45e082c101c9f6499d8f482bfe62335f58c
JIAMIJIAOYIJIAOSHOU <mallqy2657761@gmail.com>
2026-03-15 21:32:48 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
-app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+app = Flask(
+    __name__,
+    template_folder=os.path.join(BASE_DIR, "templates"),
+    static_folder=os.path.join(BASE_DIR, "static")
+)
 
 @app.route("/")
 def home():
