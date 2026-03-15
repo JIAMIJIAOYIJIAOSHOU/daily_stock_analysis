82047ef52d364a7b79a7e4a1203853b1a153032d
JIAMIJIAOYIJIAOSHOU <mallqy2657761@gmail.com>
2026-03-15 21:32:29 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,16 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-app = Flask(
-    __name__,
-    template_folder=os.path.join(BASE_DIR, "templates"),
-    static_folder=os.path.join(BASE_DIR, "static")
-)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "<h1>AI Stock Analyzer Running</h1>"
 
 handler = app
